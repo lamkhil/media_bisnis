5b473a952b0c55fc9d4147a5d2c33a826c19d1e0
lamkhil <43593728+lamkhil@users.noreply.github.com>
2023-09-29 20:24:24 +0700

    generate symlink

--- public/symlink.php
+++ public/symlink.php
@@ -2,7 +2,7 @@
 	//lokasi resources / storage
 	$targetFolder = '/var/www/html/bisnis.sumu.or.id/bisnis_storage/storage/app/public/';
 	//lokasi public_html / link shortcut
-	$linkFolder = '/var/www/html/bisnis.sumu.or.id/public/storage';
+	$linkFolder = '/var/www/html/bisnis.sumu.or.id/public/storage/';
 	symlink($targetFolder,$linkFolder);
 	echo '<br>';
 	echo 'Symlink Completed';
