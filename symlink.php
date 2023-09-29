52d4c4c59c69abc3068ae4fc795e137f9a60e890
root <lamkhil3000@gmail.com>
2023-09-29 19:44:41 +0700

    deploying

---
+++ symlink.php
@@ -0,0 +1,9 @@
+<?php
+	//lokasi resources / storage
+	$targetFolder = '/var/www/html/bisnis.sumu.or.id/bisnis_storage/storage/app/public/';
+	//lokasi public_html / link shortcut
+	$linkFolder = '/var/www/html/bisnis.sumu.or.id/storage';
+	symlink($targetFolder,$linkFolder);
+	echo '<br>';
+	echo 'Symlink Completed';
+?>
