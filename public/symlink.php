491ae3d03b2f95eea3644642dbdd6b8071b19a8b
lamkhil <43593728+lamkhil@users.noreply.github.com>
2023-09-29 20:29:08 +0700

    created link

--- public/symlink.php
+++ public/symlink.php
@@ -3,8 +3,7 @@
 	$targetFolder = '/var/www/html/bisnis.sumu.or.id/bisnis_storage/storage/app/public/';
 	//lokasi public_html / link shortcut
 	$linkFolder = '/var/www/html/bisnis.sumu.or.id/public/storage/';
-	symlink($targetFolder,$linkFolder);
+	echo symlink($targetFolder,$linkFolder);
 	echo '<br>';
-	echo readlink($linkFolder).' - '.$linkFolder;;
 	echo 'Symlink Completed';
 ?>
